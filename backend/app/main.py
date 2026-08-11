3ea04c2a1736c4b82fcdd52b9daf49c2fc0ff35f
Kamal Chahal <kamalxchahal@gmail.com>
2026-08-11 17:31:24 -0400

    Added SendGrid email service with test endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from app.services.email_service import send_email
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
     return {"message": "Hello, World!"}
+
+@app.post("/send_email")
+def emailer(receiver_email: str, subject: str, content: str):
+    result =send_email(receiver_email, subject, content)
+    if result:
+        return {"message": "Email sent successfully!"}
+    else:
+        return {"message": "Email failed to send."}
+
+    
+    
---
+++ backend/app/services/email_service.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
+api_key = os.getenv("SENDGRID_API_KEY")
+email = os.getenv("FROM_EMAIL")
+
+def send_email(receiver_email: str, subject: str, content: str):
+    message = Mail(
+        from_email=email,
+        to_emails=receiver_email,
+        subject=subject,
+        html_content=content)
+
+    try:
+        sg=SendGridAPIClient(api_key)
+        response=sg.send(message)
+        print(response.status_code)
+        return response.status_code
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+        return None
